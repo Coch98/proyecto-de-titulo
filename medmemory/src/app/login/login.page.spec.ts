ad312d4035f26f03d2941456ae8e441d0dbfb117
Cristobal Olivares <cristobal.coch@gmail.com>
2024-10-10 18:30:51 -0300

    Implementar pruebas unitarias
    - Se añadieron pruebas unitarias para RegisterPage para verificar la funcionalidad de envío y reinicio del formulario.
    - Se crearon espías para AuthService y ApiService para simular su comportamiento en las pruebas.
    - Se aseguró que los servicios se inyecten correctamente utilizando TestBed.
    - Se incluyó pruebas asíncronas con Promesas utilizando fakeAsync y tick.

--- medmemory/src/app/login/login.page.spec.ts
+++ medmemory/src/app/login/login.page.spec.ts
@@ -1,11 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms'; // Importar FormsModule
 import { LoginPage } from './login.page';
+import { AuthService } from '../services/auth.service'; // Asegúrate de que la ruta sea correcta
+import { of } from 'rxjs';
 
 describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
+  let authServiceSpy: jasmine.SpyObj<AuthService>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    // Crear un Spy para el AuthService
+    authServiceSpy = jasmine.createSpyObj('AuthService', ['loginUser']); // Reemplaza 'loginUser' con los métodos que necesites espiar
+
+    await TestBed.configureTestingModule({
+      declarations: [LoginPage],
+      imports: [
+        IonicModule.forRoot(),
+        FormsModule // Asegúrate de agregar FormsModule aquí
+      ],
+      providers: [
+        { provide: AuthService, useValue: authServiceSpy }
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LoginPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
